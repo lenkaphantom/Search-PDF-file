28bd0e0b1f2bef954edf1d085eea7f84685a18f6
lenkaphantom <96978440+lenkaphantom@users.noreply.github.com>
2024-06-23 13:07:53 +0200

    .

--- save_and_highlight_results.py
+++ save_and_highlight_results.py
@@ -1,6 +1,6 @@
 import os
 import re
-import fitz  # PyMuPDF
+import fitz
 
 def get_filename(query):
     if '"' in query:
@@ -13,7 +13,7 @@ def save_to_pdf(results, query, text_by_page, filename='search_results.pdf'):
 
     full_filename = os.path.join('rezultati', get_filename(query))
 
-    pdf = fitz.open()  # Otvara novi PDF dokument
+    pdf = fitz.open()
 
     query_words = re.findall(r'\w+', query)
 
@@ -29,32 +29,17 @@ def save_to_pdf(results, query, text_by_page, filename='search_results.pdf'):
 def highlight_pdf_page(pdf, page_text, query_words):
     page = pdf.new_page()
 
-    # Insert the text of the page
     page.insert_text((50, 100), page_text)
 
-    # Search for each query word in the text
     for word in query_words:
         word_instances = re.finditer(rf'\b{re.escape(word)}\b', page_text, flags=re.IGNORECASE)
         for instance in word_instances:
             start, end = instance.span()
             word_text = page_text[start:end]
-
-            # Find the coordinates of the word in the page
             word_instances = page.search_for(word_text)
 
-            # Highlight each found instance of the word
             for inst in word_instances:
                 highlight = page.add_highlight_annot(inst)
                 highlight.update()
 
     return page
-
-# Primer korišćenja:
-results = [(0, 0), (1, 1)]  # Primer rezultata pretrage (indeksi stranica)
-query = "Python programming"
-text_by_page = {
-    0: "This is a sample text containing Python programming keywords.",
-    1: "Python is widely used in software development."
-}
-
-save_to_pdf(results, query, text_by_page)
